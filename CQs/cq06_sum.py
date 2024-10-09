41935e9ee5f961280423d583bdf2e5de31eeec92
Andrewwb1 <aboettcher@unc.edu>
2024-10-09 13:47:34 -0400

    fixed cq06

--- CQs/cq06_sum.py
+++ CQs/cq06_sum.py
@@ -13,10 +13,6 @@ def w_sum(vals: list[float]) -> float:
     return sum
 
 
-print(w_sum([1.1, 0.9, 1.0]))
-print(w_sum([]))
-
-
 def f_sum(vals: list[float]) -> float:
     """sums all elements within the list"""
     sum: float = 0  # intializing empty float that will be used to store sum
@@ -25,17 +21,9 @@ def f_sum(vals: list[float]) -> float:
     return sum
 
 
-print(f_sum([1.1, 0.9, 1.0]))
-print(f_sum([]))
-
-
 def f_range_sum(vals: list[float]) -> float:
     """sums all elements within the list"""
     sum: float = 0  # intializing empty float that will be used to store sum
     for idx in range(0, len(vals)):
-        sum = sum + idx  # adding each element to sum
+        sum = sum + vals[idx]  # adding each element to sum
     return sum
-
-
-print(f_sum([1.1, 0.9, 1.0]))
-print(f_sum([]))
