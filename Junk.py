ed9e0a2154cf01381ae85adc4b0be50c1a75e5a3
Andrewwb1 <aboettcher@unc.edu>
2024-11-01 15:57:04 -0400

    junyed

--- Junk.py
+++ Junk.py
@@ -68,8 +68,48 @@ def double(x: int) -> int:
 def double_display(y: int):
     print(y * 2)
 
+    # double_display(2)
 
-# double_display(2)
+    # if __name__ == "__main__":
+    # print(double(3))
 
-# if __name__ == "__main__":
-# print(double(3))
+    # class Point:
+    x: float
+    y: float
+
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def dist_from_origin(self) -> float:
+        return (self.x**2 + self.y**2) ** 0.5
+
+    def translate_x(self, dx: float) -> None:
+        self.x += dx
+
+    def translate_y(self, dy: float) -> None:
+        self.y += dy
+
+    # pt1: Point = Point(2.0, 1.0)
+    # pt2: Point = Point(4.0, 6.0)
+
+    # class Line:
+    start: Point
+    end: Point
+
+    def __init__(self, start: Point, end: Point):
+        self.start = start
+        self.end = end
+
+    def get_length(self) -> float:
+        return (
+            (self.end.x - self.start.x) ** 2 + (self.end.y - self.start.y) ** 2
+        ) ** 0.5
+
+    def get_slope(self) -> float:
+        return (self.end.y - self.start.y) / (self.end.x - self.start.x)
+
+
+# Line1: Line = Line(pt1, pt2)
+
+# print(Line1.get_slope())
